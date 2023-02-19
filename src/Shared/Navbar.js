a6380b46c53bb4a19d52fa006ed51012b5e0ba2a
Masum Billah <masum.monpura@gmail.com>
2023-02-19 23:33:10 +0600

    first commit

---
+++ src/Shared/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/Logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-700">
+      <img src={logo} alt="" />
+    </nav>
+  );
+};
+
+export default Navbar;
