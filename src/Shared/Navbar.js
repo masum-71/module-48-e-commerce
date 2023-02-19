d0fb0e66e31396ed346cf26620f201955668dd7b
Masum Billah <masum.monpura@gmail.com>
2023-02-19 23:51:16 +0600

    navbar completed

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -3,8 +3,34 @@ import logo from "../images/Logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-slate-700">
+    <nav className="bg-slate-700 flex items-center justify-between pr-5 pl-5">
       <img src={logo} alt="" />
+      <div>
+        <a
+          className="no-underline mr-3 text-white hover:text-orange-500"
+          href="/order"
+        >
+          Order
+        </a>
+        <a
+          className="no-underline mr-3 text-white hover:text-orange-500"
+          href="/order-review"
+        >
+          Order Review
+        </a>
+        <a
+          className="no-underline mr-3 text-white hover:text-orange-500"
+          href="/manage-inventory"
+        >
+          Manage Inventory
+        </a>
+        <a
+          className="no-underline mr-3 text-white hover:text-orange-500"
+          href="./login"
+        >
+          Login
+        </a>
+      </div>
     </nav>
   );
 };
